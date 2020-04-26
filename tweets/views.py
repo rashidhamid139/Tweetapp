c2db672e691329c330fb18518ea5ef69ed799610
rashidhamid139 <rashidhamid139@gmail.com>
2020-04-26 09:39:58 +0000

    Making the Tweet architecture

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render, get_object_or_404
+import random
 from django.http import HttpResponse, Http404, JsonResponse
-
+from .forms import TweetForm
 from .models import Tweet
 
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, 'pages/home.html', context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={'form': form})
+
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0,100)} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
